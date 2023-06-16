a4e17b9bc3b03e7a30a716e7e0d0e0947d585d54
Abhishek <abhishkbhi@gmail.com>
2023-06-16 17:58:51 +0530

    June 16th

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Users from "./user/pages/Users";
 import MainNavigation from "./shared/components/Navigation/MainNavigation";
+import NewPlace from "./places/pages/NewPlace";
+import UserPlaces from "./places/pages/UserPlaces";
 
 function App() {
   return (
@@ -10,7 +12,9 @@ function App() {
           <MainNavigation />
           <Routes>
             <Route path="/" element={<Users />} />
-            <Route path="/places/new"></Route>
+            <Route path="/places/new" element={<NewPlace />}/>
+            <Route path="/:userId/places" element={<UserPlaces />}/>
+            
           </Routes>
         </main>
       </BrowserRouter>
--- src/user/components/UserItem.jsx
+++ src/user/components/UserItem.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import "./UserItem.css";
 import Avatar from "../../shared/components/UIElements/Avatar/Avatar";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import Card from "../../shared/components/UIElements/Card/Card";
 
 const UserItem = (props) => {
   return (
     <li className="user-item">
       <Card className="user-item__content">
-        <Link to={`/${props.id}/places`}>
+        <NavLink to={`/u${props.id}/places`}>
           <div className="user-item__image">
             <Avatar image={props.image} alt={props.name}></Avatar>
           </div>
@@ -18,7 +18,7 @@ const UserItem = (props) => {
               {props.placeCount} {props.placeCount === 1 ? "place" : "places"}
             </h3>
           </div>
-        </Link>
+        </NavLink>
       </Card>
     </li>
   );
